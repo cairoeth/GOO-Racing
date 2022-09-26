14bd214a8883f6f1dc8529095bdc164dd1a08152
cairoeth <cairoeth@protonmail.com>
2022-09-26 21:06:13 +0200

    Fix libraries
    i messed tf

--- lib/chainlink/core/cmd/helpers_test.go
+++
@@ -1,8 +0,0 @@
-package cmd
-
-import "github.com/smartcontractkit/chainlink/core/logger"
-
-// CheckRemoteBuildCompatibility exposes checkRemoteBuildCompatibility for testing.
-func (cli *Client) CheckRemoteBuildCompatibility(lggr logger.Logger, onlyWarn bool, cliVersion, cliSha string) error {
-	return cli.checkRemoteBuildCompatibility(lggr, onlyWarn, cliVersion, cliSha)
-}
--- lib/chainlink/core/services/telemetry/common.go
+++
@@ -1,9 +0,0 @@
-package telemetry
-
-import (
-	ocrtypes "github.com/smartcontractkit/libocr/commontypes"
-)
-
-type MonitoringEndpointGenerator interface {
-	GenMonitoringEndpoint(contractID string) ocrtypes.MonitoringEndpoint
-}
